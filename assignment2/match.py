53eff0b67a0ec0a3a67d0af631c78041002d1a10
Fiona Abney-McPeek <f.abneymcp@gmail.com>
2021-11-15 23:21:57 -0500

    YEAH LETS GO

--- assignment2/match.py
+++ assignment2/match.py
@@ -1,7 +1,70 @@
 import numpy as np
+from numpy import random
 from typing import List, Tuple
 
 def run_matching(scores: List[List], gender_id: List, gender_pref: List) -> List[Tuple]:
+
+
+
+
+    #does friendship matching and disregards gender preferences
+
+
+    arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+    random.shuffle(arr)
+    recievers = arr[0:5]
+    proposers = arr[5:10]
+    print(recievers)
+    print(proposers)
+    #we keep track of pairs by putting proposers in and out of the array "pairs" which is indexed by the recievers, eg. the proposer 
+    #in spot 0 is paired with the reciever in spot 0 of the recievers array
+    print(scores[0])
+    preferences = np.zeros( (5, 5))
+    for i in range(0,5):
+        for j in range(0,5):
+            preferences[i][j] = j
+        #use a variation of bubble sort to figure out each proposer's preference order
+        #here preferences[i][j] is the index of the reciever (in the recievers array) that is proposer[i]'s jth choice
+        for k in range(0, 7):
+            for j in range(0, 4):
+                if scores[proposers[i]-1][recievers[int(preferences[i][j])]-1] < scores[proposers[i]-1][recievers[int(preferences[i][j+1])]-1]:
+                    nothing = preferences[i][j]
+                    preferences[i][j] = preferences[i][j+1]
+                    preferences[i][j+1] = nothing
+    pairs = np.zeros(5)
+    matched = np.zeros(5)
+    finished = False
+    while finished == False:
+        finished = True
+        #i is the index of the proposer, matched keeps track of which proposers have been matched
+        for i in range(0,5):
+            if matched[i] == 0:
+                finished = False
+                for nthPreference in range(0,5):
+                    recieverIndex = int(preferences[i][int(nthPreference)])
+                    if pairs[recieverIndex] == 0:
+                        if matched[i] == 0:
+                            pairs[recieverIndex] = i
+                            matched[i] = 1
+                            print("hooi")
+                    if scores[recievers[recieverIndex]-1][proposers[int(pairs[recieverIndex])] - 1] < scores[recievers[recieverIndex]-1][proposers[i] - 1]:
+                        if matched[i] == 0:
+                            print("huh")
+                            matched[int(pairs[recieverIndex])] = 0
+                            pairs[int(recieverIndex)] = i
+                            matched[i] = 1
+    print("preferences: ")
+    print(preferences)
+    print("matches: " + str(pairs))   
+    print("pair 1: " + str(recievers[0]) + " " + str(proposers[int(pairs[0])]))
+    print("pair 2: " + str(recievers[1]) + " " + str(proposers[int(pairs[1])]))
+    print("pair 3: " + str(recievers[2]) + " " + str(proposers[int(pairs[2])]))
+    print("pair 4: " + str(recievers[3]) + " " + str(proposers[int(pairs[3])]))
+    print("pair 5: " + str(recievers[4]) + " " + str(proposers[int(pairs[4])]))
+
+
+
     """
     TODO: Implement Gale-Shapley stable matching!
     :param scores: raw N x N matrix of compatibility scores. Use this to derive a preference rankings.
@@ -21,7 +84,7 @@ def run_matching(scores: List[List], gender_id: List, gender_pref: List) -> List
             - What data structure can you use to take advantage of this fact when forming your matches?
         - This is by no means an exhaustive list, feel free to reach out to us for more help!
     """
-    matches = [()]
+    matches = [(recievers[0], proposers[int(pairs[0])]), (recievers[1], proposers[int(pairs[1])]), (recievers[2], proposers[int(pairs[2])]), (recievers[3], proposers[int(pairs[3])]), (recievers[4], proposers[int(pairs[4])])]
     return matches
 
 if __name__ == "__main__":
